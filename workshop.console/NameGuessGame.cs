0bfd36bf48a1df2fe6327c87b47f17236e400be2
Mona Eikli Andresen <monaeik@hotmail.com>
2025-08-06 10:36:01 +0200

    updated to two player

--- workshop.console/NameGuessGame.cs
+++ workshop.console/NameGuessGame.cs
@@ -24,19 +24,28 @@ namespace workshop.console
 
                 if (guesses.Count > 0)
                 {
-                    if (guesses.Count == _name.Length)
+                    if (_name.ToLower().Distinct().All(c => guesses.Values.Any(g => char.ToLower(g) == c)))
                     {
-                        Console.WriteLine("Well done... you guessed the name!");
+                        Console.WriteLine($"Well done... you guessed the name! The answer was: {_name}!");
                         break;
                     }
 
-                    foreach (KeyValuePair<int, char> kvp in guesses)
+                    string display = "";
+                    for (int i = 0; i < _name.Length; i++)
                     {
-                        Console.WriteLine($"Key:{kvp.Key} Value: {kvp.Value}");
+                        char c = _name[i];
+                        if (guesses.Values.Any(g => char.ToLower(g) == char.ToLower(c)))
+                        {
+                            display += c;
+                        }
+                        else
+                        {
+                            display += "-";
+                        }
                     }
-
+                    Console.WriteLine(display);
                 }
-
+                Console.WriteLine();
                 Console.WriteLine("Enter a single letter, or qqq to quit!");
                 string input = Console.ReadLine();
 
@@ -51,10 +60,18 @@ namespace workshop.console
                     continue;
                 }
                 //ignored by continue
-                if (_name.Contains(input, StringComparison.OrdinalIgnoreCase))
+                char guessedChar = char.ToLower(input[0]);
+
+                if (guesses.Values.Any(c => char.ToLower(c) == guessedChar))
+                {
+                    Console.WriteLine("You already guessed that letter!");
+                    continue;
+                }
+
+                if (_name.Contains(guessedChar, StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine("Letter is in the name!");
-                    guesses.Add(_count, char.Parse(input));
+                    guesses.Add(_count, input[0]);
                 }
             }
         }
--- workshop.console/Program.cs
+++ workshop.console/Program.cs
@@ -1,12 +1,10 @@
-﻿
+﻿using workshop.console;
 
-using workshop.console;
+Console.Write("What name should your opponent guess: ");
+string name = Console.ReadLine();
+Console.Clear();
 
 Console.WriteLine("Welcome to Name Guess!");
 
-NameGuessGame game = new NameGuessGame();
+NameGuessGame game = new NameGuessGame(name);
 game.Start();
-
-
-
-
